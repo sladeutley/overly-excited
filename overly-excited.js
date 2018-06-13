0e8baaac40eca9d899bffa46091c691aa2cb5471
Slade Utley <slade.utley@gmail.com>
2018-06-13 12:27:07 -0500

    cleaned up code

--- overly-excited.js
+++ overly-excited.js
@@ -1,66 +1,62 @@
-var sentence = ["I", "am", "very", "excited", "to", "be", "in", "this", "class"];
+// *********** OPTION 1 *****************
 
-var addExcitement = function (theWordArray) {
-    var output = "";
-    var exclamationNum = 1;
-    for (var i=0; i<theWordArray.length; i++) {
-        output += " " + theWordArray[i] ;
-    
+var sentence = [
+  "I",
+  "am",
+  "very",
+  "excited",
+  "to",
+  "be",
+  "in",
+  "this",
+  "class"
+];
+
+var addExcitement = function(theWordArray) {
+  var output = "";
+  var exclamationNum = 1;
+  for (var i = 0; i < theWordArray.length; i++) {
+    output += " " + theWordArray[i];
     if ((i + 1) % 3 === 0) {
-    for (var j = 0; j < exclamationNum; j++) {
+      for (var j = 0; j < exclamationNum; j++) {
         output += "!";
-    }
-        exclamationNum++
+      }
+      exclamationNum++;
     }
     console.log(output);
-    }
-}
+  }
+};
 
 addExcitement(sentence);
 
-// var sentence = ["I", "am", "very", "excited", "to", "be", "in", "this", "class"];
+// *********** OPTION 2 *****************
+
+// var sentence = [
+//   "I",
+//   "am",
+//   "very",
+//   "excited",
+//   "to",
+//   "be",
+//   "in",
+//   "this",
+//   "class"
+// ];
 // var exc = "!";
-// function addExcitement (theWordArray) {
-//     var output = "";
-//     console.log(theWordArray);
-//     for (var i = 0; i < theWordArray.length; i++){
-//         if(i === 0){
-//             output = theWordArray[i] + " ";
-//             console.log(output);
-//         } else if ((i + 1) % 3 === 0) {
-//             output += theWordArray[i] + exc.repeat((i+1)/3) + " ";
-//             console.log(output);
-//         } else {
-//             output += theWordArray[i] + " ";
-//             console.log(output);
-//         }
+// function addExcitement(theWordArray) {
+//   var output = "";
+//   for (var i = 0; i < theWordArray.length; i++) {
+//     if (i === 0) {
+//       output = theWordArray[i] + " ";
+//       console.log(output);
+//     } else if ((i + 1) % 3 === 0) {
+//       output += theWordArray[i] + exc.repeat((i + 1) / 3) + " ";
+//       console.log(output);
+//     } else {
+//       output += theWordArray[i] + " ";
+//       console.log(output);
 //     }
+//   }
 // }
 
-// addExcitement(sentence)
-
-
-
-// const SENTENCES = {
-//     sentence :  ["I", "am", "very", "excited", "to", "be", "in", "this", "class"], butts :     ["hey", "hey", "hey", "hey", "hey", "hey", "hey", 
-//                 "hey", "hey"]
-//             };
-// var exc = "!";
-// function addExcitement (theWordArray) {
-//     var output = "";
-//     console.log(theWordArray);
-//     for (var i = 0; i < theWordArray.length; i++){
-//         if(i === 0){
-//             output = theWordArray[i] + " ";
-//             console.log(output);
-//         } else if ((i + 1) % 3 === 0) {
-//             output += theWordArray[i] + exc.repeat((i+1)/3) + " ";
-//             console.log(output);
-//         } else {
-//             output += theWordArray[i] + " ";
-//             console.log(output);
-//         }
-//     }
-// }
-// addExcitement(SENTENCES.sentence);
-// addExcitement(SENTENCES.butts);
+// addExcitement(sentence);
